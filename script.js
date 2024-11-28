402fcae7c44049e559e5e5dc56dfec878931d3e5
Edgars Jaunskalze <edgarsneko@gmail.com>
2024-11-28 15:11:58 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const body = document.body
+const h1 = document.createElement("h1")
+const div = document.createElement("div")
+const h3 = document.createElement("h3")
+
+
+
+
+h1.innerText = "Hello Renār"
+body.append(h1)
+
+h3.innerText = "reku mans darbs par video saturu"
+body.append(h3)
+
+div.innerHTML = '<iframe width=";1343;" height=";480;" src="https://www.youtube.com/embed/y17RuWkWdn8"></iframe>'
+body.append(div)
+
+//Man bija dažas grūtības izdomāt uzdevumu ko es varētu izdarīt un arī pirmo reizi darot līdzi paņēma 60 min :c un vel tgd ir bikin grūti tur rakstī
+
